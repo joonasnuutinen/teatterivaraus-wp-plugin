b423c833826d256f0622a2962dbc4f39cdf6d25a
Joonas Nuutinen <joonas@joonasnuutinen.com>
2019-11-27 16:02:02 +0200

    Add plugin header data

--- teatterivaraus.php
+++ teatterivaraus.php
@@ -1,4 +1,10 @@
 <?php
 /**
  * Plugin Name: Teatterivaraus
+ * Plugin URI:  https://www.teatterivaraus.fi/
+ * Description: Teatterivaraus plugin for managing ticket reservations in WordPress.
+ * Version:     1.0
+ * License:     GPLv2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: teatterivaraus
  */
